b27bdd1ea029c4f9e6e5dc090b3372836bfb6a2c
David Stanley <dstanley@uoguelph.ca>
2020-06-25 11:36:58 -0400

    update render

--- z_knit_book.R
+++ z_knit_book.R
@@ -3,8 +3,8 @@
 
 bookdown::render_book("index.Rmd", output_format = "bookdown::pdf_book")
 
-# bookdown::render_book("index.Rmd", "bookdown::gitbook")
+ bookdown::render_book("index.Rmd", "bookdown::gitbook")
 # bookdown::serve_book(dir = ".", output_dir = "_book", preview = TRUE, in_session = TRUE, quiet = FALSE)
-bookdown::serve_book(dir = ".", output_dir = "_book", preview = TRUE, in_session = TRUE, quiet = FALSE)
+#bookdown::serve_book(dir = ".", output_dir = "_book", preview = TRUE, in_session = TRUE, quiet = FALSE)
 
 # To stop the server, run servr::daemon_stop(1) or restart your R session
