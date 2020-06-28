7d0b53c371090accaa44ace9dcb96a69e75ef792
David Stanley <dstanley@uoguelph.ca>
2020-06-28 07:36:46 -0400

    update

--- z_knit_book.R
+++ z_knit_book.R
@@ -1,10 +1,9 @@
 
 # Reminder: restart R first
-
+bookdown::render_book("index.Rmd", "bookdown::gitbook")
 bookdown::render_book("index.Rmd", output_format = "bookdown::pdf_book")
 bookdown::render_book("index.Rmd", output_format = "bookdown::epub_book")
 
- bookdown::render_book("index.Rmd", "bookdown::gitbook")
 # bookdown::serve_book(dir = ".", output_dir = "_book", preview = TRUE, in_session = TRUE, quiet = FALSE)
 #bookdown::serve_book(dir = ".", output_dir = "_book", preview = TRUE, in_session = TRUE, quiet = FALSE)
 
